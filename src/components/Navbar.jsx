5ba531a1fbb22e4e0dae26b49494c1735c3b579f
Your Name <abhimanyuraj134@gmail.com>
2025-12-25 04:33:34 +0530

    Fix navbar logo import path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
+import { logo } from "../assets";
 import { Menu, X } from "lucide-react"; // Using Lucide for icons
 
 const Navbar = () => {
@@ -38,7 +39,7 @@ const Navbar = () => {
                         window.scrollTo(0, 0);
                     }}
                 >
-                    <img src="/src/assets/logo.svg" alt="logo" className="w-9 h-9 object-contain" />
+                    <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
                     <p className='text-white text-[18px] font-bold cursor-pointer flex '>
                         Abhimanyu &nbsp;
                         <span className='sm:block hidden'> | Web Developer</span>
