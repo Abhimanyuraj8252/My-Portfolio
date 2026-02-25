0df81d6de142f889c2ba5f5cbc0b79620e0bed98
Abhimanyuraj8252 <abhimanyuraj134@gmail.com>
2026-02-26 01:28:48 +0530

    Update admin credentials in seed script

--- server/seed.js
+++ server/seed.js
@@ -4,13 +4,18 @@ import bcrypt from 'bcryptjs';
 const prisma = new PrismaClient();
 
 async function main() {
-    const hash = await bcrypt.hash('admin123', 10);
-    await prisma.user.upsert({
-        where: { email: 'admin@admin.com' },
-        update: {},
-        create: { email: 'admin@admin.com', password: hash },
+    const hash = await bcrypt.hash('@Abhi98018', 10);
+    const user = await prisma.user.upsert({
+        where: { email: 'abhimanyuraj134@gmail.com' },
+        update: {
+            password: hash
+        },
+        create: {
+            email: 'abhimanyuraj134@gmail.com',
+            password: hash
+        },
     });
-    console.log('Admin created: admin@admin.com / admin123');
+    console.log('Admin updated/created: abhimanyuraj134@gmail.com / @Abhi98018');
 }
 
 main()
